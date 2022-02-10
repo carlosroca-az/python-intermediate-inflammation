c9c70791dddba1296d13c1a60b159ddfc3f3b46c
Carlos Roca <carlos.roca@astrazeneca.com>
2022-02-10 15:31:31 +0000

    Improve docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,27 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: matrix with inflammation data (rows: patients, columns: days)
+    :returns: array with daily means of inflammation for each patient
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: matrix with inflammation data (rows: patients, columns: days)
+    :returns: array with maximum value of inflammation for each patient
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
-    return np.min(data, axis=0)
+    """Calculate the daily min of a 2D inflammation data array.
 
+    :param data: matrix with inflammation data (rows: patients, columns: days)
+    :returns: array with minimum value of inflammation for each patient
+    """
+    return np.min(data, axis=0)
