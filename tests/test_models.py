d86797160afc6c238fe6a57f7d4169e0456ee6f0
Carlos Roca <carlos.roca@astrazeneca.com>
2022-02-16 17:17:46 +0000

    Add preconditions to patient_normalise()

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
 """Tests for statistics functions within the Model layer."""
 
+import numpy as np
 import numpy.testing as npt
 import pytest
 
@@ -49,6 +50,45 @@ def test_daily_min(test, expected):
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
-
     with pytest.raises(TypeError):
         daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected, raises",
+    [
+        ('hi there',
+         None,
+         TypeError),
+        (3.1416,
+         None,
+         TypeError),
+        ([1, 2, 3, 4, 5, 6],
+         None,
+         ValueError),
+        ([[-1, 2, 3], [-4, 5, 6], [7, 8, 9]],
+         [[0, 0.67, 1], [0, 0.83, 1], [0.78, 0.89, 1]],
+         ValueError),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+         [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+         None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+         [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+         None),
+        ([[1, float('nan'), 3], [4, 5, 6], [7, 8, float('nan')]],
+         [[0.33, 0, 1], [0.67, 0.83, 1], [0.88, 1, 0]],
+         None),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+         [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+         None)
+    ])
+def test_patient_normalise(test, expected, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if isinstance(test, list):
+        test = np.array(test)
+    if raises:
+        with pytest.raises(raises):
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
